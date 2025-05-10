799cfae9b789b128b74bf7559528f8a315f9e561
Nathan Beddoe <nbedd2@protonmail.com>
2025-05-10 13:39:11 +1000

    fix container

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,9 @@
 <x-layouts.app.header :title="$title ?? null">
-    <flux:main>
-        <flux:container>
+    <flux:main container>
         <flux:heading size="xl">Welcome to Laravel Starter Kits</flux:heading>
-        <flux:heading size="lg" class="mt-8">Most Downloaded Starter Kits</flux:heading>
+        <flux:heading size="lg" class="mt-8">Favourite Starter Kits</flux:heading>
         <div class="mt-4">
             <livewire:most-downloaded-starter-kit-list />
         </div>
-        </flux:container>
     </flux:main>
 </x-layouts.app.header>
